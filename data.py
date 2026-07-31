e0c633d128a890823d270a78c8b165e209e8839d
Rajan Belwal <belwalrajan72@gmail.com>
2026-07-31 20:41:41 +0530

    Add grading system based on input marks

--- data.py
+++ data.py
@@ -1,3 +1,17 @@
 name = "rajan"
 age = 23 
 is_student = True 
+
+
+marks = int(input("Enter your marks: " ))
+
+if marks >= 90 :
+  print("A+")
+elif marks >=80 and marks < 90 :
+  print("B+") 
+elif marks >= 60 and marks < 80 : 
+  print("C+") 
+elif marks >= 33 and marks < 60 : 
+  print("D")
+else: 
+  print("fail") 
